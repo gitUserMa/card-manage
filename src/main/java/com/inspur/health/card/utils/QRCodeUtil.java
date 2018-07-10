ed2d7f95f78d7a38fe55e7f7457a110b572f0f3a
ma.liang <ma.liang@inspur.com>
2018-07-10 11:26:12 +0800

    二维码图片生成工具

---
+++ src/main/java/com/inspur/health/card/utils/QRCodeUtil.java
@@ -0,0 +1,81 @@
+package com.inspur.health.card.utils;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Hashtable;
+
+/**
+ * 二维码工具类
+ */
+public class QRCodeUtil {
+
+    private static final String CHARSET = "utf-8";
+    /**
+     * 二维码尺寸
+     */
+    private static final int QRCODE_SIZE = 100;
+    /**
+     * 二维码格式
+     */
+    private static final String FILE_TYPE = "jpg";
+
+    /**
+     * 生成二维码图片
+     *
+     * @param content
+     *                    二维码内容
+     * @param filePath
+     *                    文件路径
+     * @throws Exception
+     */
+    public static void createImage(String content, String filePath) throws Exception {
+        File myFile = new File(filePath);
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE, hints);
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000
+                        : 0xFFFFFFFF);
+            }
+        }
+        ImageIO.write(image, FILE_TYPE, myFile);
+    }
+
+
+
+    public static void createImage(String content, HttpServletResponse response) throws Exception {
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE, hints);
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000
+                        : 0xFFFFFFFF);
+            }
+        }
+        ImageIO.write(image, "jpg", response.getOutputStream());
+    }
+}
