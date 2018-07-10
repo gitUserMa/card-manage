04e75979de209b8cd75377aaae2c3ea9a345e95a
ma.liang <ma.liang@inspur.com>
2018-07-10 11:23:51 +0800

    二维码图片生成

---
+++ src/main/java/com/inspur/health/card/controller/QRCodeController.java
@@ -0,0 +1,48 @@
+package com.inspur.health.card.controller;
+
+import com.inspur.health.card.utils.QRCodeUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ma.liang on 2018/7/7.
+ *
+ * @author ma.liang
+ */
+@Controller
+@RequestMapping("/tools")
+public class QRCodeController {
+
+    /**
+     * 生成二维码，返回到页面上
+     *
+     * @param response
+     */
+    @RequestMapping(value = "/getQRCode", method = {RequestMethod.POST, RequestMethod.GET})
+    public void getErWeiCode(@RequestParam("content") String content, HttpServletResponse response) {
+        ServletOutputStream stream = null;
+        try {
+            if (content != null && !"".equals(content)) {
+                QRCodeUtil.createImage(content, response);
+                stream = response.getOutputStream();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.flush();
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
