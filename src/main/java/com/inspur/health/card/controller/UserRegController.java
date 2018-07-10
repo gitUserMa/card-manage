4fa388346598f4dc3dbe7de8e3023f5a995478de
ma.liang <ma.liang@inspur.com>
2018-07-10 15:23:43 +0800

    去注释

--- src/main/java/com/inspur/health/card/controller/UserRegController.java
+++ src/main/java/com/inspur/health/card/controller/UserRegController.java
@@ -1,25 +1,25 @@
-//package com.inspur.health.card.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import com.inspur.health.card.data.RegInfo;
-//import com.inspur.health.card.service.IUserRegService;
-//
-//@Controller
-//@RequestMapping("/users")
-//public class UserRegController {
-//
-//	@Autowired
-//	private IUserRegService userRegService;
-//
-//	@RequestMapping(method = RequestMethod.POST)
-//	@ResponseBody
-//	public String regUser(@RequestBody RegInfo info) {
-//		return userRegService.regUser(info);
-//	}
-//}
+package com.inspur.health.card.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.inspur.health.card.data.RegInfo;
+import com.inspur.health.card.service.IUserRegService;
+
+@Controller
+@RequestMapping("/users")
+public class UserRegController {
+
+	@Autowired
+	private IUserRegService userRegService;
+
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public String regUser(@RequestBody RegInfo info) {
+		return userRegService.regUser(info);
+	}
+}
